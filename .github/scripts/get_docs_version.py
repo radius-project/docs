e0386c3c51c718307b2c4b8e461bb7a2a70c6e7c
Aaron Crawfis <Aaron.Crawfis@microsoft.com>
2023-05-18 15:05:18 -0700

    Specify ref for radius checkout so CLI docs are versioned (#578)
    * Specify ref for radius checkout
    
    * Fix order

--- .github/scripts/get_docs_version.py
+++ .github/scripts/get_docs_version.py
@@ -9,11 +9,13 @@
 # We set the environment variable REL_CHANNEL based on the kind of build. This is used for
 # versioning of our assets.
 #
-# DOCS_CHANNEL is:
-# 'edge': for most builds
-# '1.0': for PR builds
-
 # DOCS_CHANNEL is used to upload assets to different paths
+# 'edge' when targeting edge branch
+# '1.0' when targeting a release branch
+#
+# RELEASE_BRANCH is used when referencing the full release version
+# 'main' when targeting edge branch
+# '1.0' when targeting a release branch
 
 import os
 import sys
@@ -22,19 +24,24 @@ gitRefName = os.getenv("GITHUB_BASE_REF")
 print("GITHUB_BASE_REF: {}".format(gitRefName))
 
 with open(os.getenv("GITHUB_ENV"), "a") as githubEnv:
-    version = "DOCS_CHANNEL=edge"
+    channel = "DOCS_CHANNEL=edge"
+    release_branch = "RELEASE_BRANCH=main"
 
     if gitRefName is None:
-        print("This is not running in github, GITHUB_REF_NAME is null. Assuming a local build. Setting DOCS_CHANNEL to 'edge'")
-        version = "DOCS_CHANNEL=edge"
+        print("This is not running in github, GITHUB_REF_NAME is null. Assuming a local build. Setting DOCS_CHANNEL to 'edge' and RELEASE_BRANCH to 'main'")
     elif gitRefName.lower() == 'edge':
-        print("This is an edge build, setting DOCS_CHANNEL to 'edge'")
-        version = "DOCS_CHANNEL=edge"
+        print("This is an edge build, setting DOCS_CHANNEL to 'edge' and RELEASE_BRANCH to 'main'")
     elif gitRefName.startswith('v'):
-        print("This is a release build, setting DOCS_CHANNEL to version")
-        versionNumber = gitRefName.split('v')[1]
-        version = "DOCS_CHANNEL=" + versionNumber
+        print("This is a release build, setting DOCS_CHANNEL and RELEASE_BRANCH to version")
+        channel = "DOCS_CHANNEL=" + gitRefName.split('v')[1]
+        release_branch = "RELEASE_BRANCH=" + gitRefName + '.0'
+    else:
+        print("This is a non-standard build, setting DOCS_CHANNEL to 'edge' and RELEASE_BRANCH to 'main'")
 
-    print("Setting: {}".format(version))
-    githubEnv.write(version + "\n")
+    print("Setting DOCS_CHANNEL: {}".format(channel))
+    githubEnv.write(channel + "\n")
+
+    print("Setting RELEASE_BRANCH: {}".format(release_branch))
+    githubEnv.write(release_branch + "\n")
+
     sys.exit(0)
