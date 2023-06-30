8066a032348105fae21d508c4ea7ebffdd1c8b50
Aaron Crawfis <Aaron.Crawfis@microsoft.com>
2023-06-30 12:36:55 -0700

    Set user_id for preview members

--- docs/static/js/clarity.js
+++ docs/static/js/clarity.js
@@ -4,7 +4,11 @@ $(document).ready(function() {
         url: "/.auth/me",
         success: function(data) {
             if (data[0] && data[0].user_id) {
+                // Set identifier as hashed user id
                 window.clarity("identify", data[0].user_id);
+
+                // Set user attributes for signed in (preview) users
+                window.clarity("set", "user_id", data[0].user_id);
             }
         }
     });
