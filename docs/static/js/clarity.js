96c79843a29fa06feba8c51de2b6bead442cf88e
Aaron Crawfis <Aaron.Crawfis@microsoft.com>
2023-07-05 08:42:54 -0700

    Revert user_id

--- docs/static/js/clarity.js
+++ docs/static/js/clarity.js
@@ -6,9 +6,6 @@ $(document).ready(function() {
             if (data[0] && data[0].user_id) {
                 // Set identifier as hashed user id
                 window.clarity("identify", data[0].user_id);
-
-                // Set user attributes for signed in (preview) users
-                window.clarity("set", "user_id", data[0].user_id);
             }
         }
     });
