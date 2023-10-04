481fbdf0427866319a285c534ee72dc573be6cb7
Aaron Crawfis <Aaron.Crawfis@microsoft.com>
2023-10-04 19:28:36 +0000

    Simplify npm install command

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-# Clone radius repo
-git clone https://github.com/radius-project/radius.git
-
 # Initialize docs package
-cd docs
+pushd docs
 npm install
-
-# Generate Swagger docs
-mkdir -p ./docs/static/swagger
-cp -r ./radius/swagger ./docs/static/
+popd
